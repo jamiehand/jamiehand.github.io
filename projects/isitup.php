ff700d0cad28422a0dae326d38603e7cf27cfa80
Jamie Hand <hand.jamie@gmail.com>
2016-06-21 13:39:37 -0700

    Make isitup complicated again

--- projects/isitup.php
+++ projects/isitup.php
@@ -22,14 +22,14 @@ USAGE
 # Grab some of the values from the slash command, create vars for post back to Slack
 
 
-/*
+
 
 
 $command = $_POST['command'];
 $text = $_POST['text'];
 $token = $_POST['token'];
 # Check the token and make sure the request is from our team
-if($token != 'vnLfaOlI7natbpU5tKQBm5dQ'){ #replace this with the token from your slash command configuration page
+if($token != 'jqMbQvusb0dKyTbHwH2hXAx5'){ #replace this with the token from your slash command configuration page
   $msg = "The token for the slash command doesn't match. Check your script.";
   die($msg);
   echo $msg;
@@ -76,7 +76,4 @@ if($ch_response === FALSE){
 echo $reply;
 
 
-*/
 
-
-echo "hello!";
